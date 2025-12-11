82b136438a8e4604d4edce5266ed1b345496e3de
AbdikarimMHassan <abdikarimmh98@gmail.com>
2025-12-11 02:46:35 +0000

    add remote state via s3 and state locking via dyanmodb

---
+++ infra/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.2.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "abdikarimh-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform_locks"
+
+  }
+}
