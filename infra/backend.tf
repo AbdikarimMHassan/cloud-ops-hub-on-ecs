1e1f35550472d36bb653bd4068ca74a64624e8df
AbdikarimMHassan <abdikarimmh98@gmail.com>
2025-12-14 06:21:34 +0000

    add reqired version

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,15 +1,17 @@
 terraform {
+  required_version = "~> 1.0" 
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.2.0"
     }
   }
+  
   backend "s3" {
     bucket         = "abdikarimh-state-bucket"
     key            = "terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform_locks"
-
   }
 }
